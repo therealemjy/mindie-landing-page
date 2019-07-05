0c8c560bd8355a106fbe3bdb5688cb9430ae886e
Maxime Julian <maxime.julian.creations@gmail.com>
2019-07-05 14:05:53 +0100

    finish integrating stripe

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,9 @@ export default {
     publicKey: 'pk_test_Ppo54I5KwwE0QOPJPsTcB6W4',
   },
   api: {
-    setupIntentUrl: 'http://localhost:3050/setup-intent',
+    createCustomerUrl:
+      'http://localhost:5000/mindie-landing-test/us-central1/createCustomer',
+    setupIntentUrl:
+      'http://localhost:5000/mindie-landing-test/us-central1/setupIntent',
   },
 };
--- src/containers/CardDetailsForm/index.tsx
+++ src/containers/CardDetailsForm/index.tsx
@@ -5,7 +5,14 @@ import { injectStripe } from 'react-stripe-elements';
 import config from 'config';
 import CardDetailsForm from 'components/CardDetailsForm';
 
-const ContainedCardDetailsForm: React.SFC = props => {
+export interface Props {
+  stripe: any; // TODO: define typing
+}
+
+const ContainedCardDetailsForm: React.SFC<Props> = ({
+  stripe,
+  ...otherProps
+}) => {
   const [clientSecret, setClientSecret] = useState<undefined | string>(
     undefined
   );
@@ -29,13 +36,42 @@ const ContainedCardDetailsForm: React.SFC = props => {
       });
   }, []);
 
-  const handleSubmit = () => {
-    console.log('SUBMIT');
-    console.log(cardElementRef);
+  const handleSubmit = async () => {
+    // Save card details
+    const { setupIntent, error } = await stripe.handleCardSetup(
+      clientSecret,
+      cardElementRef,
+      {
+        payment_method_data: {
+          billing_details: {
+            name: 'Quentin de Montargis',
+          },
+        },
+      }
+    );
+
+    if (error) {
+      console.log(error);
+      setError(error);
+    }
+
+    // Create customer
+    Axios({
+      method: 'post',
+      url: config.api.createCustomerUrl,
+      data: {
+        // TODO: get real email address
+        email: 'contact@maxime-julian.com',
+        paymentMethodId: setupIntent.payment_method,
+      },
+    }).catch(error => {
+      console.log(error);
+      setError(error);
+    });
   };
 
   if (error) {
-    return <>Something wrong happened :(</>;
+    return <>Something went wrong :(</>;
   }
 
   if (!clientSecret) {
@@ -44,7 +80,7 @@ const ContainedCardDetailsForm: React.SFC = props => {
 
   return (
     <CardDetailsForm
-      {...props}
+      {...otherProps}
       onSubmit={handleSubmit}
       onReady={setCardElementRef}
     />
