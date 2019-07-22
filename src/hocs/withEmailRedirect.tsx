040dead44ee46f8ad450f13b8e869cf075c8c739
Maxime Julian <maxime.julian.creations@gmail.com>
2019-07-22 22:40:57 +0100

    fix issue with window object (2)

--- src/hocs/withEmailRedirect.tsx
+++ src/hocs/withEmailRedirect.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { navigate } from 'gatsby';
 
 import getSearchParam from 'utils/getSearchParam';
 
 function withEmailRedirect<T>(Component: React.ComponentType<T>) {
   return (componentProps: any) => {
-    let email =
-      typeof window !== undefined &&
-      getSearchParam(window.location.href, 'email');
+    let email: string | undefined;
 
-    if (!email) {
-      navigate('/');
-    }
+    useEffect(() => {
+      email = getSearchParam(window.location.href, 'email') || undefined;
+
+      if (!email) {
+        navigate('/');
+      }
+    }, []);
 
     return <Component {...componentProps} email={email} />;
   };
