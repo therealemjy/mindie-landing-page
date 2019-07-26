052a67e59ac2c4facd8ea10d54cac548e26abd73
Maxime Julian <maxime.julian.creations@gmail.com>
2019-07-26 15:03:18 +0100

    update success page content

--- src/hocs/withSteps.tsx
+++ src/hocs/withSteps.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const withSteps = (Component: React.SFC) => (componentProps: any) => {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(2);
 
   return <Component {...componentProps} step={step} setStep={setStep} />;
 };
